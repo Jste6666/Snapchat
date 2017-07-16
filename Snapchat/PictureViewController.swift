90bcb3edffd71f69f7ebf047bf8757270152288a
Justin Stewart <discgolfstew@gmail.com>
2017-07-16 05:14:34 -0400

    Finished section 7

---
+++ Snapchat/PictureViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PictureViewController.swift
+//  Snapchat
+//
+//  Created by Justin Stewart on 7/16/17.
+//  Copyright © 2017 Justin Stewart. All rights reserved.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func nextTapped(_ sender: Any) {
+    }
+
+}
