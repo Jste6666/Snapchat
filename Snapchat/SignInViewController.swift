08451fbff712c2fbbe8484fd58b00909350c5381
Justin Stewart <discgolfstew@gmail.com>
2017-07-16 03:39:41 -0400

    Finished section 4

--- Snapchat/SignInViewController.swift
+++ Snapchat/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  Snapchat
 //
 //  Created by Justin Stewart on 7/16/17.
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignInViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func turnUpTapped(_ sender: Any) {
+        
     }
-
+    
 
 }
 
