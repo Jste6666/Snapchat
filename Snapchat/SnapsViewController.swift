63e983a28dc630c0cde10bef0a723565d345d6a0
Justin Stewart <discgolfstew@gmail.com>
2017-07-17 03:10:23 -0400

    Finished section 15

--- Snapchat/SnapsViewController.swift
+++ Snapchat/SnapsViewController.swift
@@ -7,17 +7,56 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+import FirebaseAuth
 
-class SnapsViewController: UIViewController {
+class SnapsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var snaps : [Snap] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
 
         // Do any additional setup after loading the view.
+        
+        Database.database().reference().child("users").child(Auth.auth().currentUser!.uid).child("snaps").observe(DataEventType.childAdded, with: {(snapshot) in
+            print(snapshot)
+            
+            let snap = Snap()
+            
+            snap.imageURL = (snapshot.value as? NSDictionary)?["imageURL"] as? String ?? ""
+            snap.from = (snapshot.value as? NSDictionary)?["from"] as? String ?? ""
+            snap.descrip = (snapshot.value as? NSDictionary)?["description"] as? String ?? ""
+
+            
+            self.snaps.append(snap)
+            
+            self.tableView.reloadData()
+            
+        })
     }
 
     @IBAction func logoutTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return snaps.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell =  UITableViewCell()
+        
+        let snap = snaps[indexPath.row]
+        
+        cell.textLabel?.text = snap.from
+        
+        return cell
     }
 
 }
---
+++ Snapchat/snap.swift
@@ -0,0 +1,15 @@
+//
+//  snap.swift
+//  Snapchat
+//
+//  Created by Justin Stewart on 7/17/17.
+//  Copyright © 2017 Justin Stewart. All rights reserved.
+//
+
+import Foundation
+
+class Snap {
+    var imageURL = ""
+    var descrip = ""
+    var from = ""
+}
